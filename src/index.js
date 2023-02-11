1aa6c5f9f2e93ffd9ab26a665ed0caffa4d5b58f
Marcelo Camillo <marcelocamillo18@gmail.com>
2023-02-11 20:30:48 -0300

    Object Types

--- src/index.js
+++ src/index.js
@@ -3,22 +3,39 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
+      user: User!
+      users: [User!]!
+    }
+
+    type User {
       id: ID!
-      name: String!
-      age: Int!
-      average: Float!
-      married: Boolean!
-      arrayString: [String!]!
+      username: String
     }
   `,
   resolvers: {
     Query: {
-      id: () => '9aacb090-5d58-4670-a8c1-8e057c83ff64',
-      name: () => 'Marcelo Camillo',
-      age: () => 29,
-      average: () => 50.55,
-      married: () => false,
-      arrayString: () => [],
+      user: () => {
+        return {
+          id: '9aacb090-5d58-4670-a8c1-8e057c83ff64',
+          username: 'marcelocamillo',
+        };
+      },
+      users: () => {
+        return [
+          {
+            id: '9aacb090-5d58-4670-a8c1-8e057c83ff64',
+            username: 'marcelocamillo',
+          },
+          {
+            id: 'b354a810-4a65-41f3-9874-3fa8a4aee348',
+            username: 'user1',
+          },
+          {
+            id: '1aadc399-5ced-4e0f-a729-baba280358dc',
+            username: 'user2',
+          },
+        ];
+      },
     },
   },
 });
