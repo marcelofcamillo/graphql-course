938ab47ca80b40869e7c094c55420238b2bfcef9
Marcelo Camillo <marcelocamillo18@gmail.com>
2023-02-11 20:21:37 -0300

    Scalar Types

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
-      hi: String
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
     }
   `,
   resolvers: {
     Query: {
-      hello: () => {
-        return 'Hello again';
-      },
-      hi: () => {
-        return 'Hi';
-      },
+      id: () => '9aacb090-5d58-4670-a8c1-8e057c83ff64',
+      name: () => 'Marcelo Camillo',
+      age: () => 29,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => [],
     },
   },
 });
