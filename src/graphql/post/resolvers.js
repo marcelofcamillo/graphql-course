efbc218ac73329d8e2fdc02e483e11e74bb80104
Marcelo Camillo <marcelocamillo18@gmail.com>
2023-02-18 18:47:54 -0300

    Resolvendo o tipo Posts

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,25 +1,13 @@
-const post = () => {
-  return {
-    id: () => 'cfae5b00-dacb-412a-bb04-3c7e60317741',
-    title: () => 'Title title',
-  };
+const post = async (_, { id }, { getPosts }) => {
+  const response = await getPosts(`/${id}`);
+
+  return response.json();
 };
 
-const posts = () => {
-  return [
-    {
-      id: '67f5e646-6993-48b6-b92c-c933d394fc15',
-      title: 'Title 1',
-    },
-    {
-      id: 'a45fdfcc-0a17-4a5d-a906-4a6ef3bf3212',
-      title: 'Title 2',
-    },
-    {
-      id: '4515dcb4-6549-4772-85a2-bd1d2fb3a0c1',
-      title: 'Title 3',
-    },
-  ];
+const posts = async (_, __, { getPosts }) => {
+  const response = await getPosts();
+
+  return response.json();
 };
 
 export const postResolvers = {
